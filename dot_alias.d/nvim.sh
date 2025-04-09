11930904ae5a6d95dbf93939b2bbc0d30c218b71
David A Brown <dabrown645@gmail.com>
2025-04-08 22:49:35 -0700

    🆕 NEW: Added few of my scripts and some alias updates

--- dot_alias.d/nvim.sh
+++ dot_alias.d/nvim.sh
@@ -1,4 +1,4 @@
-alias nn="NVIM_APPNAME=nvim nvim"
-alias n="NVIM_APPNAME=new.nvim nvim"
+alias n="NVIM_APPNAME=nvim nvim"
+alias nn="NVIM_APPNAME=new.nvim nvim"
 alias nk="NVIM_APPNAME=kickstart.nvim nvim"
 alias ndiff="nvim -d"
